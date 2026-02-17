faa5851b8556cb46eb3ae259e1855593574dc633
Siddhant Nair <55419437+sidnair23@users.noreply.github.com>
2026-02-16 21:10:16 -0500

    Created a python script to successfully load

---
+++ python/python_databricks_to_azure/main.py
@@ -0,0 +1,38 @@
+'''
+Docstring for python.python_databricks_to_azure.main
+'''
+import os
+from dotenv import load_dotenv
+from azure.ai.textanalytics import TextAnalyticsClient
+from azure.core.credentials import AzureKeyCredential
+
+def authenticate_client():
+    '''
+    Authenticates the client for Azure Text Analytics API using credentials from .env file.
+    Returns:
+        TextAnalyticsClient: An authenticated client for Azure Text Analytics API.
+    '''
+    load_dotenv()  # Load environment variables from .env file
+    endpoint = os.getenv("LanguageServiceEndpoint")
+    api_key = os.getenv("LanguageServiceAPI")
+    
+    if endpoint and api_key:
+        print("Successfully loaded environment variables.")
+        
+    else:
+        raise ValueError("Please set the LanguageServiceEndpoint and LanguageServiceAPI in the .env file.")
+
+
+def main():
+    '''
+    Main function to authenticate the client and perform text analytics operations.
+    '''
+    try:
+        client = authenticate_client()
+        
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+if __name__ == "__main__":
+    main()
